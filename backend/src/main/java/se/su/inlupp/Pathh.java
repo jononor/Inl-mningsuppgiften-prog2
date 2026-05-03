d5e8e1a745677435f6ffe58066d860b14e59bb24
jononor <john.nordstrom500@outlook.com>
2026-05-03 09:38:14 +0900

    Edges klassen färdig, Path klassen inte klar, DSFPathFinder inte klar

--- backend/src/main/java/se/su/inlupp/Pathh.java
+++ backend/src/main/java/se/su/inlupp/Pathh.java
@@ -1,14 +1,18 @@
 package se.su.inlupp;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Pathh<T> implements Path<T> {
     private T startNode;
     private T endNode;
+    private ListGraph<T> graph;
 
-    public Pathh(T startNode, T endNode) {
+    public Pathh(T startNode, T endNode, ListGraph<T> graph) {
         this.startNode = startNode;
         this.endNode = endNode;
+        this.graph = graph;
     }
 
     @Override
@@ -25,19 +29,24 @@ public class Pathh<T> implements Path<T> {
     public int getTotalWeight() {
         int totalWeight = 0;
 
-        //... kod som itererar tills slutnoden
-
         return totalWeight;
     }
 
     @Override
     public List<Edge<T>> getEdges() {
-        return null; // inte klar
+        //return edges;
+        return null;
     }
 
     @Override
     public List<T> getNodes() {
-        return null; //inte klar
+        return null;
+    }
+
+    @Override
+    public Iterator<Edge<T>> iterator() {
+        //return graph.iterator();
+        return null;
     }
 
     @Override
