77bd57874ca5883402be70d8d0d1869d1bcb794b
jononor <john.nordstrom500@outlook.com>
2026-05-04 12:33:36 +0900

    Path är klar, DSFPathFinder är klar

--- backend/src/main/java/se/su/inlupp/DFSPathFinder.java
+++ backend/src/main/java/se/su/inlupp/DFSPathFinder.java
@@ -1,10 +1,39 @@
 package se.su.inlupp;
 
+import java.util.ArrayList;
+
 public class DFSPathFinder<T> implements PathFinder<T> {
 
   @Override
   public Path<T> findPath(Graph<T> graph, T from, T to) {
+    ArrayList<T> allNodes = (ArrayList<T>) graph.getNodes();
+    if (allNodes.contains(from) && allNodes.contains(to)) {
+      ArrayList<T> visitedNodes = new ArrayList<T>();
+      ArrayList<Edge<T>> visitedEdges = new ArrayList<>();
+      visit(from, visitedNodes, visitedEdges , graph);
+      if (visitedNodes.contains(to)) {
+        return new Pathh(from, to, visitedNodes, visitedEdges , graph);
+      }
+
+    } else {
+      throw new NullPointerException("Noderna måste finnas i grafen")
+    }
+
     return null;
   }
+
+  private void visit(T from, ArrayList<T> visitedNodes, ArrayList<Edge<T>> visitedEdges, Graph<T> graph) {
+    visitedNodes.add(from);
+    ArrayList<Edge<T>> edges = (ArrayList<Edge<T>>) graph.getEdgesFrom(from);
+    for (Edge<T> edge : edges ) {
+      visitedEdges.add(edge);
+      T destination = edge.getDestination();
+      if(!visitedNodes.contains(destination)) {
+        visit(destination, visitedNodes, visitedEdges, graph);
+      }
+    }
+  }
+
+
 }
 
--- backend/src/main/java/se/su/inlupp/Pathh.java
+++ backend/src/main/java/se/su/inlupp/Pathh.java
@@ -1,17 +1,23 @@
 package se.su.inlupp;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 public class Pathh<T> implements Path<T> {
     private T startNode;
     private T endNode;
-    private ListGraph<T> graph;
+    private Collection<T> visitedNodes;
+    private Collection<Edge<T>> visitedEdges;
+    private Graph<T> graph;
 
-    public Pathh(T startNode, T endNode, ListGraph<T> graph) {
+
+    public Pathh(T startNode, T endNode, Collection<T> visitedNodes, Collection<Edge<T>> visitedEdges, Graph<T> graph) {
         this.startNode = startNode;
         this.endNode = endNode;
+        this.visitedNodes = visitedNodes;
+        this.visitedEdges = visitedEdges;
         this.graph = graph;
     }
 
@@ -28,30 +34,38 @@ public class Pathh<T> implements Path<T> {
     @Override
     public int getTotalWeight() {
         int totalWeight = 0;
-
+        for (Edge<T> edge : visitedEdges) {
+            totalWeight += edge.getWeight();
+        }
         return totalWeight;
     }
 
     @Override
     public List<Edge<T>> getEdges() {
-        //return edges;
-        return null;
+        List<Edge<T>> edges = new ArrayList<>();
+        for (Edge<T> edge : visitedEdges) {
+            edges.add(edge);
+        }
+        return edges;
     }
 
     @Override
     public List<T> getNodes() {
-        return null;
+        List<T> nodes = new ArrayList<>();
+        for (T node : visitedNodes) {
+            nodes.add(node);
+        }
+        return nodes;
     }
 
     @Override
     public Iterator<Edge<T>> iterator() {
-        //return graph.iterator();
-        return null;
+        return visitedEdges.iterator();
     }
 
     @Override
     public String toString() {
-        return null; //inte klar
+        String  str = "Start node:" + getStart() + " End node:" + getEnd() + " All nodes:" + getNodes() + " TotalWeight:" + getTotalWeight();
     }
 
 }
