b6922cfbeb9efa705bddf40f26772a3f57e578be
jononor <john.nordstrom500@outlook.com>
2026-05-06 00:30:17 +0900

    Path är klar, DSFPathFinder är klar

--- backend/src/main/java/se/su/inlupp/DFSPathFinder.java
+++ backend/src/main/java/se/su/inlupp/DFSPathFinder.java
@@ -16,9 +16,8 @@ public class DFSPathFinder<T> implements PathFinder<T> {
       }
 
     } else {
-      throw new NullPointerException("Noderna måste finnas i grafen")
+      throw new NullPointerException("Noderna måste finnas i grafen");
     }
-
     return null;
   }
 
