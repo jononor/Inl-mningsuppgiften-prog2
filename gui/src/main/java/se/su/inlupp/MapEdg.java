fd13809c52e83beb8fe3f42595b193a3319d036e
jononor <john.nordstrom500@outlook.com>
2026-08-17 00:48:19 +0900

    updaterade edge wrapper

--- gui/src/main/java/se/su/inlupp/MapEdg.java
+++ gui/src/main/java/se/su/inlupp/MapEdg.java
@@ -5,19 +5,38 @@ import java.util.List;
 public class MapEdg {
     private List<Edge<CityPlace>> edges;
 
+    private Edge<CityPlace> edge;
+
     public MapEdg(List<Edge<CityPlace>> edges) {
         this.edges = edges;
     }
 
 
+    public MapEdg (Edge<CityPlace> edge) {
+        this.edge = edge;
+    }
 
+    public int getWeight() {
+        return edge.getWeight();
+    }
+
+    public void setWeight(int weight) {
+        edge.setWeight(weight);
+    }
+
+    public CityPlace getDestination() {
+        return edge.getDestination();
+    }
+
+    public String getName() {
+        return edge.getName();
+    }
 
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for ( Edge<CityPlace> edge : edges) {
             if (edge instanceof Edgee<CityPlace>) {
-                //Edgee<CityPlace> edgee = (Edgee<CityPlace>) edge;
                 sb.append(((Edgee<CityPlace>) edge).toString());
                 sb.append("\n");
             }
